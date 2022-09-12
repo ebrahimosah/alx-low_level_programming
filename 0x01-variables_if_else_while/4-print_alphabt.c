b222bfb775cdeb779dbcc3dd6e1cceed4ba43a0b
ibrahim osah <ibrahimosah2@gmail.com>
2022-09-12 14:27:11 -0700

    okay

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <ctype.h>
+/**
+ * main - Program to print alphabet letter followed by new line
+ * Return: return 0
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
