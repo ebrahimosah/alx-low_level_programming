eb71282ebfc6498834c3ff508fa2d3e6ea565e4c
ibrahim osah <ibrahimosah2@gmail.com>
2022-10-07 05:54:46 -0700

    okay

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,42 +3,65 @@
 
 
 /**
- * * _calloc - allocate memory using malloc and initialize it to zero
+ * *string_nconcat - Concatenates two strings using at
  *
- * *@nmemb: number of elements
+ * *most an inputted number of bytes.
  *
- * *@size: size of the memory block to be allocated
+ * *@s1: The first string.
+ *
+ * *@s2: The second string.
+ *
+ * *@n: The maximum number of bytes of s2 to concatenate to s1.
  *
  * *
  *
- * *Return: poiner to the address of the memory block
+ * *Return: If the function fails - NULL.
+ *
+ * *Otherwise - a pointer to the concatenated space in memory.
  *
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
 
-		char *block;
+		char *concat;
 
-		unsigned int i;
+		unsigned int len = n, index;
 
 
 
-		if (nmemb == 0 || size == 0)
+		if (s1 == NULL)
 
+			s1 = "";
+
+
+
+		if (s2 == NULL)
+
+			s2 = "";
+
+
+
+		for (index = 0; s1[index]; index++)
+			len++;
+
+
+		concat = malloc(sizeof(char) * (len + 1));
+
+
+
+		if (concat == NULL)
 			return (NULL);
 
-		block = malloc(nmemb * size);
 
-		if (block != NULL)
+		len = 0;
 
-		{
-			for (i = 0; i < (nmemb * size); i++)
-				block[i] = 0;
-			return (block);
-		}
+		for (index = 0; s1[index]; index++)
+			concat[len++] = s1[index];
 
-		else
-			return (NULL);
+		for (index = 0; s2[index] && index < n; index++)
+			concat[len++] = s2[index];
 
+		concat[len] = '\0';
+		return (concat);
 }
